89206ab8ac4b718d96bcb416f7f38159a9f23367
Deathspike <meister.deathspike@outlook.com>
2019-06-21 20:50:50 +0200

    Fix openapi2ts to include missing redirects

--- scripts/openapi2ts.js
+++ scripts/openapi2ts.js
@@ -5,6 +5,7 @@ const json2ts = require('json-schema-to-typescript');
 const contents = fs.readFileSync(process.argv[2], 'utf8');
 const definitions = {};
 const properties = {};
+const redirects = [];
 const validationContext = api.createValidationContext(JSON.parse(contents));
 
 [validationContext.components.schemas, validationContext.requests, validationContext.responses].forEach((values) => {
@@ -12,23 +13,25 @@ const validationContext = api.createValidationContext(JSON.parse(contents));
     if (definitions[propertyName]) throw new Error(`Duplicate definition: ${propertyName}`);
     definitions[propertyName] = values[propertyName];
     properties[propertyName] = {$ref: `#/definitions/${propertyName}`};
-    update(true, definitions[propertyName]);
+    update(propertyName, definitions[propertyName]);
   }
 });
 
-function update(isRoot, schema) {
+function update(root, schema) {
   if (schema.$ref) {
-    schema.$ref = schema.$ref.replace(/^#\/components\/schemas\/(.*)$/, '#/definitions/$1');
+    const match =  schema.$ref.match(/^#\/components\/schemas\/(.*)$/);
+    const reference = match && match[1];
+    if (reference) schema.$ref = `#/definitions/${reference}`;
+    if (reference && root) redirects.push({root, reference});
   } else if (Array.isArray(schema)) {
     schema.forEach(update);
   } else if (typeof schema === 'object') {
-    Object.keys(schema).forEach(x => update(false, schema[x]));
-    if (isRoot || typeof schema.type === 'string') {
-      schema.additionalProperties = false;
-    }
+    Object.keys(schema).forEach(x => update(null, schema[x]));
+    if (root || typeof schema.type === 'string') schema.additionalProperties = false;
   }
 }
 
 json2ts.compile({properties, definitions}, '').then((result) => {
   console.log(result);
+  redirects.forEach((redirect) => console.log(`export type ${redirect.root} = ${redirect.reference};`));
 });
